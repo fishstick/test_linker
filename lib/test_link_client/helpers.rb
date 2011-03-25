6d8d96f3d46d44639d7bfbd57b910806682257c5
Steve Loveless <steve.loveless@gmail.com>
2011-03-25 00:33:04 -0700

    Fixed bug in #test_project_id_for and added #find_test_plans and #api_version

--- lib/test_link_client/helpers.rb
+++ lib/test_link_client/helpers.rb
@@ -1,3 +1,26 @@
-class Helpers
-  # To change this template use File | Settings | File Templates.
+module TestLinkClient::Helpers
+  def test_project_id_for test_project_name
+    project = projects.find { |project| project["name"] == test_project_name }
+    project["id"]
+  end
+
+  # @return [Array] An array of test plans that match the Regexp.
+  def find_test_plans test_project_id, regex
+    list = []
+
+    test_plan_list = project_test_plans(test_project_id).first
+
+    test_plan_list.each_value do |test_plan_info|
+      if test_plan_info["name"] =~ regex
+        list << test_plan_info
+      end
+    end
+
+    list
+  end
+
+  def api_version
+    about =~ /Testlink API Version: (.+) initially/
+    $1
+  end
 end
