fc2fb53d52ede22fa1361016fdad66b979cb5a9b
Steve Loveless <steve.loveless@gmail.com>
2011-03-29 23:22:22 -0700

    Moved common step to env/common.rb

--- features/step_definitions/get_info_steps.rb
+++ features/step_definitions/get_info_steps.rb
@@ -1,16 +1,3 @@
-Given /^I have a TestLink server with API version (.+)$/ do |api_version|
-  if api_version == "1.0"
-    url = "http://ubuntu-desktop/testlink/"
-    dev_key = "b8c144a536f8233d24b04b8268bfac34"
-  else
-    url = "http://testlink/"
-    dev_key = "90b7941411928ae0a84d19f365a01a63"
-  end
-  @server = TestLinkClient.new(url, dev_key)
-  @server.api_version.should == api_version
-  @server.about.should match /#{api_version}/
-end
-
 Given /^I know the name of a project$/ do
   @test_project_name = @server.projects.first["name"]
   @test_project_name.should_not be_nil
