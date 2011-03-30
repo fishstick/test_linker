6518cfc59da1b43e76ceca40659d7855935d4b3c
Steve Loveless <steve.loveless@gmail.com>
2011-03-29 18:33:37 -0700

    Added dev_key for the 1.9 test server

--- features/step_definitions/get_info_steps.rb
+++ features/step_definitions/get_info_steps.rb
@@ -1,10 +1,11 @@
 Given /^I have a TestLink server with API version (.+)$/ do |api_version|
   if api_version == "1.0"
     url = "http://ubuntu-desktop/testlink/"
+    dev_key = "b8c144a536f8233d24b04b8268bfac34"
   else
     url = "http://testlink/"
+    dev_key = "90b7941411928ae0a84d19f365a01a63"
   end
-  dev_key = "90b7941411928ae0a84d19f365a01a63"
   @server = TestLinkClient.new(url, dev_key)
   @server.api_version.should == api_version
   @server.about.should match /#{api_version}/
