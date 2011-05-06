9781f18b96508b9a2768f449c2fe0d8d2dc119e6
Steve Loveless <steve.loveless@gmail.com>
2011-05-05 23:13:19 -0700

    Only use simplecov if RUBY_VERSION > '1.9'

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,9 @@
-require 'simplecov'
-SimpleCov.start do
-  add_group "Library", "lib/"
+if RUBY_VERSION > '1.9'
+  require 'simplecov'
+  
+  SimpleCov.start do
+    add_group "Library", "lib/"
+  end
 end
 
 require File.expand_path(File.dirname(__FILE__) + '/../../lib/test_linker')
