09005fa1429dc7c5d42aa817317fa00ecadef176
Steve Loveless <steve.loveless@gmail.com>
2011-05-06 00:31:43 -0700

    Added test for #say_hello

--- spec/test_linker/wrapper_spec.rb
+++ spec/test_linker/wrapper_spec.rb
@@ -7,16 +7,39 @@ def register_body(body)
 end
 
 describe TestLinker::Wrapper do
+  before do
+    body = "<?xml version=\"1.0\"?>\n<methodResponse>\n  <params>\n    <param>\n      <value>\n        <string> Testlink API Version: 1.0 initially written by Asiel Brumfield\n with contributions by TestLink development Team</string>\n      </value>\n    </param>\n  </params>\n</methodResponse>\n"
+    register_body(body)
+    @tl = TestLinker.new "http://testing", "devkey"
+  end
+  
   after :each do
     FakeWeb.clean_registry
   end
   
   describe "#about" do
     it "gets the about string from the server" do
-      body = "<?xml version=\"1.0\"?>\n<methodResponse>\n  <params>\n    <param>\n      <value>\n        <string> Testlink API Version: 1.0 initially written by Asiel Brumfield\n with contributions by TestLink development Team</string>\n      </value>\n    </param>\n  </params>\n</methodResponse>\n"
+      @tl.about.should == " Testlink API Version: 1.0 initially written by Asiel Brumfield\n with contributions by TestLink development Team"
+    end
+  end
+  
+  describe "#say_hello" do
+    it "gets the server to say 'Hello!'" do
+      body = <<-XML
+<?xml version=\"1.0\"?>
+<methodResponse>
+  <params>
+    <param>
+      <value>
+        <string>Hello!</string>
+      </value>
+    </param>
+  </params>
+</methodResponse>
+
+XML
       register_body(body)
-      tl = TestLinker.new "http://testing", "devkey"
-      tl.about.should == " Testlink API Version: 1.0 initially written by Asiel Brumfield\n with contributions by TestLink development Team"
+      @tl.say_hello.should == "Hello!"
     end
   end
 end
