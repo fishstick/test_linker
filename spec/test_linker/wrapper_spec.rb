a3d070c5e92215a61cd7a1b27acb5623ca15976d
Steve Loveless <steve.loveless@gmail.com>
2011-05-08 20:47:40 -0700

    Added test for #test_plans and fixed other tests to use Ints instead of Strings

--- spec/test_linker/wrapper_spec.rb
+++ spec/test_linker/wrapper_spec.rb
@@ -40,6 +40,7 @@ XML
   describe "#projects" do
     it "gets a list of projects on the server" do
       body = <<-XML
+<?xml version=\"1.0\"?>
 <methodResponse>
   <params>
     <param>
@@ -55,7 +56,7 @@ XML
   <member><name>prefix</name><value><string>W_UDI</string></value></member>
   <member><name>tc_counter</name><value><string>0</string></value></member>
   <member><name>option_automation</name><value><string>0</string></value></member>
-  <member><name>name</name><value><string>Warehouse - UDI5000</string></value></member>
+  <member><name>name</name><value><string>Warehouse</string></value></member>
 </struct></value>
   <value><struct>
   <member><name>id</name><value><string>465934</string></value></member>
@@ -79,9 +80,89 @@ XML
       
       register_body(body)
       @tl.projects.should == [
-          {"id"=>"335241", "notes"=>"", "color"=>"", "active"=>"1", "option_reqs"=>"0", "option_priority"=>"0", "prefix"=>"W_UDI", "tc_counter"=>"0", "option_automation"=>"0", "name"=>"Warehouse - UDI5000"}, 
-          {"id"=>"465934", "notes"=>"", "color"=>"", "active"=>"1", "option_reqs"=>"1", "option_priority"=>"1", "prefix"=>"ztest", "tc_counter"=>"142", "option_automation"=>"1", "name"=>"z_test"}
+          {:id=>335241, :notes=>"", :color=>"", :active=>1, :option_reqs=>0,
+              :option_priority=>0, :prefix=>"W_UDI", :tc_counter=>0,
+              :option_automation=>0, :name=>"Warehouse"}, 
+          {:id=>465934, :notes=>"", :color=>"", :active=>1, :option_reqs=>1,
+              :option_priority=>1, :prefix=>"ztest", :tc_counter=>142,
+              :option_automation=>1, :name=>"z_test"}
         ]
+    end
+  end
+  
+  describe "#test_suites_for_test_plan" do
+    it "gets a list of test suites" do
+      body = <<-XML
+<?xml version=\"1.0\"?>
+<methodResponse>
+  <params>
+    <param>
+      <value>
+        <array><data>
+  <value><struct>
+  <member><name>name</name><value><string>Basic Functionality</string></value></member>
+  <member><name>id</name><value><string>431197</string></value></member>
+</struct></value>
+  <value><struct>
+  <member><name>name</name><value><string>Other Functionality</string></value></member>
+  <member><name>id</name><value><string>429939</string></value></member>
+</struct></value>
+</data></array>
+      </value>
+    </param>
+  </params>
+</methodResponse>
+
+      XML
+
+      register_body(body)
+      @tl.test_suites_for_test_plan(576096).should == [
+          {:name=>"Basic Functionality", :id=>431197},
+          {:name=>"Other Functionality", :id=>429939}
+      ]
+    end
+  end
+  
+  describe "#test_plans" do
+    it "gets a list of test plans" do
+      body = <<-XML
+<?xml version=\"1.0\"?>
+<methodResponse>
+  <params>
+    <param>
+      <value>
+        <array><data>
+  <value><struct>
+  <member><name>466122</name><value><struct>
+  <member><name>id</name><value><string>466122</string></value></member>
+  <member><name>name</name><value><string>App 2.5.0</string></value></member>
+  <member><name>notes</name><value><string></string></value></member>
+  <member><name>active</name><value><string>1</string></value></member>
+  <member><name>testproject_id</name><value><string>392797</string></value></member>
+</struct></value></member>
+  <member><name>576096</name><value><struct>
+  <member><name>id</name><value><string>576096</string></value></member>
+  <member><name>name</name><value><string>App 2.5.1</string></value></member>
+  <member><name>notes</name><value><string></string></value></member>
+  <member><name>active</name><value><string>1</string></value></member>
+  <member><name>testproject_id</name><value><string>392797</string></value></member>
+</struct></value></member>
+</struct></value>
+</data></array>
+      </value>
+    </param>
+  </params>
+</methodResponse>
+
+      XML
+
+      register_body(body)
+      @tl.test_plans(392797).should == [
+          466122=>
+              {:id=>466122, :name=>"App 2.5.0", :notes=>"", :active=>1, :testproject_id=>392797},
+          576096=>
+              {:id=>576096, :name=>"App 2.5.1", :notes=>"", :active=>1, :testproject_id=>392797}
+      ] 
     end
   end
 end
